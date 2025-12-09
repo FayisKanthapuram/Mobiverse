bb6b5f45061df86d55805cda15131bbf5de3c6b2
FayisKanthpuram <fayiskanthapuram@gmail.com>
2025-12-09 15:56:36 +0530

    feat:implemented cloudinary defaults

--- src/config/cloudinaryDefaults 2.js
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_USER_AVATAR = "https://res.cloudinary.com/dlqronc2z/image/upload/v1765254735/user-avatar_wyifzm.jpg"
-export const LOGO="https://res.cloudinary.com/dlqronc2z/image/upload/v1765256298/logo_fjuwdg.png"
-export const LOGONAME='https://res.cloudinary.com/dlqronc2z/image/upload/v1765256298/logo_name_tb4rtm.png'
